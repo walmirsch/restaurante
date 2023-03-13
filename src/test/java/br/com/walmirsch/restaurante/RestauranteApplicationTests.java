23ca1e12a5f06965475b8080dd9e60fbc832328f
Walmir Schultz <walmirsch@gmail.com>
2023-03-12 22:13:51 -0300

    Início apenas Spring Boot dependencies

---
+++ src/test/java/br/com/walmirsch/restaurante/RestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.walmirsch.restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
